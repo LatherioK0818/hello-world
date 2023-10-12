204f7c1d112fdf4b57eef1f5b415d09550115149
Latherio Kidd <kidd.latherio@gmail.comS>
2023-10-12 12:48:37 -0400

    updating java to match html page

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-alert( "Welcome to my page !");
-
-const promptAnswer = prompt('What is your favorite Game ?');
-
-console.log(promptAnswer);
+            // Show a welcome alert
+            alert("Welcome to my page!");
+    
+            // Prompt the user for their favorite game
+            const promptAnswer = prompt('What is your favorite game?');
+    
+            // Log the user's response to the console
+            console.log(promptAnswer);
+            //Displaying the Answer
+            document.write('User\'s favorite game is: ' + promptAnswer);
