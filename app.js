1fa37f01e7eed8eaa737ce210240cb4a0ce9e028
Latherio Kidd <kidd.latherio@gmail.comS>
2023-10-13 10:46:33 -0400

    adding the lab 8  requirement to the java file

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
 'use strict';
-
 // Function to ask for the favorite game
 function askFavGame() {
-    let gameAnswer = prompt('What is your favorite game?');
-    document.write('User\'s favorite game is: ' + gameAnswer +" .");
-    console.log(gameAnswer);
-    if (gameAnswer === '') {
+    let gameAnswer;
+    do {
+        gameAnswer = prompt('What is your favorite game?');
+        if (gameAnswer === '') {
+            alert('Please enter your Favorite game')
+        }
         gameAnswer = prompt('Please enter your favorite game: ');
     }
+    while (gameAnswer === "");
+    document.write('User\'s favorite game is: ' + gameAnswer + " .");
+    console.log(gameAnswer);
     return gameAnswer;
 }
-
 // Function to ask about the gaming system
 function askSystem() {
-    let consoleAnswer = prompt("What's your favorite gaming System?");
-    document.write("User's favorite gaming system is: " + consoleAnswer +" .");
-    
-    if (consoleAnswer.toLowerCase() === 'playstation') {
+    let consoleAnswer;
+    do {
+        consoleAnswer = prompt("What's your favorite gaming System?");
+        if (consoleAnswer === '') {
+            alert('Please enter your favorite gaming system.');
+        }  
+    }
+    while (consoleAnswer === "");
+    if(consoleAnswer.toLowerCase() === 'playstation') {
         let psAnswer = prompt(" Did you like the PS5? ");
         if (psAnswer.toLowerCase() === 'yes') {
             document.write("So do I! Welcome, friend!");
         } else if (psAnswer.toLowerCase() === 'no') {
             document.write(" BOO!!!!!!!!!!!");
         }
-    } else if (consoleAnswer.toLowerCase() === 'xbox') {
+    } if(consoleAnswer.toLowerCase() === 'xbox') {
         document.write(" Hello, my nemesis. You may enter at your own risk! ");
     } else if (consoleAnswer.toLowerCase() === 'nintendo') {
         document.write(" Alright, move it along, Mario! ");
     }
-    
+    document.write(" User's favorite gaming system is: " + consoleAnswer + " .");
+    console.log(consoleAnswer);   
     return consoleAnswer;
 }
+function giveRating() {
+    // Ask for the user's rating
+    let userNumber;
+    do {
+        userNumber = prompt('What would you rate Sonic? Choose a number between 1-5. 1 being the lowest and 5 being the highest.');
+        userNumber = parseInt(userNumber);
+    } while (isNaN(userNumber) || userNumber < 1 || userNumber > 5);
 
-// Call the functions
+    // Display the images based on the rating
+    for (let n = 1; n <= userNumber; n++) {
+        document.write('<img src="shadow.jpg">');
+    }
+    console.log(userNumber);
+    return userNumber;
+}
+
+giveRating();
 askFavGame();
 askSystem();
 
