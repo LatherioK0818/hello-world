c66175841fa473ab0a7a21efa48d36ca273689ab
Latherio Kidd <kidd.latherio@gmail.comS>
2023-10-12 20:49:51 -0400

    Class 7 updates to Lab

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-            // Show a welcome alert
-            alert("Welcome to my page!");
+'use strict';
+
+// Function to ask for the favorite game
+function askFavGame() {
+    let gameAnswer = prompt('What is your favorite game?');
+    document.write('User\'s favorite game is: ' + gameAnswer +" .");
+    console.log(gameAnswer);
+    if (gameAnswer === '') {
+        gameAnswer = prompt('Please enter your favorite game: ');
+    }
+    return gameAnswer;
+}
+
+// Function to ask about the gaming system
+function askSystem() {
+    let consoleAnswer = prompt("What's your favorite gaming System?");
+    document.write("User's favorite gaming system is: " + consoleAnswer +" .");
     
-            // Prompt the user for their favorite game
-            const promptAnswer = prompt('What is your favorite game?');
+    if (consoleAnswer.toLowerCase() === 'playstation') {
+        let psAnswer = prompt(" Did you like the PS5? ");
+        if (psAnswer.toLowerCase() === 'yes') {
+            document.write("So do I! Welcome, friend!");
+        } else if (psAnswer.toLowerCase() === 'no') {
+            document.write(" BOO!!!!!!!!!!!");
+        }
+    } else if (consoleAnswer.toLowerCase() === 'xbox') {
+        document.write(" Hello, my nemesis. You may enter at your own risk! ");
+    } else if (consoleAnswer.toLowerCase() === 'nintendo') {
+        document.write(" Alright, move it along, Mario! ");
+    }
     
-            // Log the user's response to the console
-            console.log(promptAnswer);
-            //Displaying the Answer
-            document.write('User\'s favorite game is: ' + promptAnswer);
+    return consoleAnswer;
+}
+
+// Call the functions
+askFavGame();
+askSystem();
+
